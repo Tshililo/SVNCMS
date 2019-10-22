656a914ab60c818ab3b34d69f1f22bede6b3ec1c
b.mutheiwana@gmail.com <Mutheiwana27>
2019-10-22 17:23:55 +0200

    Created Slides show ClassicBBQ

--- cms/Controllers/MainMenuController.cs
+++ cms/Controllers/MainMenuController.cs
@@ -31,19 +31,19 @@ namespace cms.Controllers
 
         }
 
-        public ActionResult SavePayments(string PeopleId, string numberofplates, string emailId = "", string phoneNo = "")
+        public ActionResult SavePayments(string PeopleId, string numberofplates, string emailId = "", string phoneNo = "", string Occasionvalue = "", string EventsType = "")
         {
-            insert(PeopleId, numberofplates, emailId, phoneNo);
+            insert(PeopleId, numberofplates, emailId, phoneNo, Occasionvalue);
 
             return PartialView("CreateMainMenuEditPartial");
 
         
         }
 
-        public void SendSMS(string PhoneNo, String RefNo)
+        public void SendSMS(string PhoneNo, string RefNo, string Occasionvalue = "", string EventsType = "")
         {
             string to, msg;
-            msg = "";
+            msg = "Dear Customer. You have Booked a Chef for Occasion: " + Occasionvalue + " for " + EventsType + " Event" + RefNo + ""; ;
             to = PhoneNo;
             WebClient client = new WebClient();
             // Add a user agent header in case the requested URI contains a query.
@@ -62,7 +62,7 @@ namespace cms.Controllers
 
         }
 
-        private void SendEmail(string EmailAddress , string RefNo )
+        private void SendEmail(string EmailAddress , string RefNo, string Occasionvalue = "", string EventsType = "")
         {
             MailMessage Msg = new MailMessage();
             // Sender e-mail address.
@@ -70,7 +70,7 @@ namespace cms.Controllers
             // Recipient e-mail address.
             Msg.To.Add(EmailAddress);
             Msg.Subject = "Dear Client";
-            Msg.Body = "Kindly Receive Proof of Booking your Reference No: " + RefNo + "";
+            Msg.Body = "You have Booked a Chef for Occasion: " + Occasionvalue + " for "  + EventsType + " Event" + RefNo + "";
             Msg.IsBodyHtml = true;
             // your remote SMTP server IP.
             SmtpClient smtp = new SmtpClient();
@@ -88,7 +88,7 @@ namespace cms.Controllers
             return result;
         }
 
-        private string insert(string PeopleId, string numberofplates, string emailId = "", string phoneNo = "")
+        private string insert(string PeopleId, string numberofplates, string emailId = "", string phoneNo = "", string Occasionvalue = "", string EventsType = "")
         {
             try
             {
@@ -96,16 +96,15 @@ namespace cms.Controllers
                 string NoOfPeople = PeopleId;
                 string NoOfPlates = numberofplates;
                 string Date = DateTime.Now.ToString();
-
+                string occasionvalue = Occasionvalue;
                 string RefNo = "Uba - " + RandomString(9, false);
-
+                string Eventstype = EventsType;
                 string FNUmber = ReplaceLastOccurrence(phoneNo, "0", "+27") +",";
 
                 //This is my insert query in which i am taking input from the user through windows forms  
-                string Query = "insert into ubachef.Payments(ObjId,NoOfPeople,NoOfPlates,Date,RefNo) values('" + ObjId + "','" + NoOfPeople + "','" + NoOfPlates + "','" + Date + "','" + RefNo + "');";
-                //This is  MySqlConnection here i have created the object and pass my connection string.  
+                string Query = "insert into ubachef.Payments(ObjId,NoOfPeople,NoOfPlates,Date,RefNo,emailId,phoneNo,Occasion,EventsType) values('" + ObjId + "','" + 
+                    NoOfPeople + "','" + NoOfPlates + "','" + Date + "','" + RefNo + "','" + emailId + "','" + FNUmber + "','" + occasionvalue + "','" + EventsType + "');";
                 MySqlConnection MyConn2 = new MySqlConnection(gg.MyConnection2);
-                //This is command class which will handle the query and connection object.  
                 MySqlCommand MyCommand2 = new MySqlCommand(Query, MyConn2);
                 MySqlDataReader MyReader2;
                 MyConn2.Open();
@@ -118,7 +117,8 @@ namespace cms.Controllers
                 MyConn2.Close();
 
 
-                SendEmail(emailId, RefNo);
+                SendEmail(emailId, RefNo, occasionvalue, Eventstype);
+                SendSMS(emailId, RefNo, occasionvalue, Eventstype);
                 return "Procord Processed";
             }
             catch (Exception ex)
