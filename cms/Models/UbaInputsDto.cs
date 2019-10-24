bc70e290ff84bffd04a93683319e9e68f8bff27b
b.mutheiwana@gmail.com <Mutheiwana27>
2019-10-24 15:59:19 +0200

    check in All windows

---
+++ cms/Models/UbaInputsDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace cms.Models
+{
+    public class Response
+    {
+        public string Data { get; set; }
+        public int ErrorCode { get; set; }
+        public string ErrorMsg { get; set; }
+    }
+
+
+    public class UbaInputs
+    {
+        public string Data { get; set; }
+        public int ErrorCode { get; set; }
+        public string ErrorMsg { get; set; }
+    }
+}
